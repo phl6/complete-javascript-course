5a860b7a3b01c1b89ae0a20e97e28a136f4539eb
phl6 <louisli1997@gmail.com>
2020-11-03 19:15:30 +0800

    Ch29. Done coding challege 4

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -191,80 +191,12 @@
 // var johnMark = 'John and MArk';
 // var if = 23;
 
+//----------------------------------------------------------------------------------------------------
+//Chapter29-Coding Challenge#4
 
 
+const bill = prompt(`How much is the bill`);
+Number(bill)
+const tip = bill >= 50 || bill <=300 ? bill * 0.15 : bill * 0.20;
 
-/*****************************
-* Variable mutation and type coercion
-*/
-/*
-var firstName = 'John';
-var age = 28;
-
-// Type coercion
-console.log(firstName + ' ' + age);
-
-var job, isMarried;
-job = 'teacher';
-isMarried = false;
-
-console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
-
-// Variable mutation
-age = 'twenty eight';
-job = 'driver';
-
-alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
-
-var lastName = prompt('What is his last Name?');
-console.log(firstName + ' ' + lastName);
-*/
-
-
-
-/*****************************
-* Basic operators
-*/
-/*
-var year, yearJohn, yearMark;
-now = 2018;
-ageJohn = 28;
-ageMark = 33;
-
-// Math operators
-yearJohn = now - ageJohn;
-yeahMark = now - ageMark;
-
-console.log(yearJohn);
-
-console.log(now + 2);
-console.log(now * 2);
-console.log(now / 10);
-
-
-// Logical operators
-var johnOlder = ageJohn < ageMark;
-console.log(johnOlder);
-
-
-// typeof operator
-console.log(typeof johnOlder);
-console.log(typeof ageJohn);
-console.log(typeof 'Mark is older tha John');
-var x;
-console.log(typeof x);
-*/
-
-
-
-/*****************************
-* Operator precedence
-*/
-var now = 2020;
-var yearJohn = 1989;
-var fullAge = 18;
-
-var isFullAge = now - yearJohn >= fullAge;
-console.log(isFullAge);
-
-
+console.log(`Tip of the bill is ${tip}`);
