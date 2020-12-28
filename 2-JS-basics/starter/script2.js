ec1f9a4d699500fcd808f415b2b8f3401899b767
phl6 <guitarlouisli1997@gmail.com>
2020-12-28 15:08:12 +0800

    updated script2.js

--- 2-JS-basics/starter/script2.js
+++ 2-JS-basics/starter/script2.js
@@ -30,14 +30,17 @@ const appleJuice = fruitProcessor(5,0);
 console.log(appleJuice);
 
 console.log(fruitProcessor(3, 5));
-
-//filter (Learn from youtube)
-//filter uses higher order function inside the bracket
+//==================================================================================================================================================
+//                                                                  LEARN FROM YOUTUBE STARTS
+//==================================================================================================================================================
+//Merging two arrays
 const arr1 = ['z','a','c','d','b'];
 const arr2 = [1, 2, 3, 4];
-const arr_merge = [...arr1, ...arr2];
+const arr_merge = [...arr1, ...arr2]; 
 console.log(arr_merge);
 
+//FILTER 
+//filter uses higher order function inside the bracket
 const animals = ["owl", "frog", "canary", "duck", "duck", "goose", "owl"];
 // Parameters example: 'owl', 0, ['owl', 'frog', 'canary', 'duck', 'duck', 'goose', 'owl']
 const filteredAnimals = animals.filter((animal, index, arr) => arr.indexOf(animal) == index);
@@ -45,8 +48,8 @@ console.log(filteredAnimals);
 
 const numbers = [1 ,-1, 2, 3];
 const positiveNums = numbers.filter(n => n >= 0);
-
 console.log(positiveNums); 
+
 console.log(arr1.sort());
 
 let vals = [5, 4, 9, 2, 1];
@@ -58,4 +61,6 @@ function isEven(num){
 //the following 2 are the same
 console.log('using normal function: ' + numbers.filter(isEven));
 console.log('using arrow function: ' + numbers.filter((n) => n%2 == 0));
-
+//==================================================================================================================================================
+//                                                                  LEARN FROM YOUTUBE ENDS
+//==================================================================================================================================================
