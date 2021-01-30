52f6a9622870ef52adfff286a70ca828a5d5a809
phl6 <louisli1997@gmail.com>
2021-01-31 01:39:49 +0800

    Started a new very USEFUL chapter about destructuring array, object, etc.

---
+++ Sec9Data_Structures_Operators/103destructuringArray.js
@@ -0,0 +1,74 @@
+'use strict';
+//========================================================================================================
+//Section9 - 103 - Destructuring Array
+//========================================================================================================
+
+// Data needed for first part of the section
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  order: function(starterIndex, mainIndex){
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]; //return an array
+  },
+};
+//--------------------------------------------------------------------------------------------------------
+const arr = [0,1,2];
+
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+ 
+//destructing the array from the right side
+const [x, y, z] = arr; 
+console.log(x,y,z);
+
+//extract the first two elements of the array
+const [first1, second1] = restaurant.categories;
+console.log(first1, second1);
+
+//extract only the first and third elements
+let [main, ,secondary] = restaurant.starterMenu;
+console.log(main, secondary); //Focaccia Garlic Bread
+
+//--------------------------------------------------------------------------------------------------------
+//Swapping Variables
+//--------------------------------------------------------------------------------------------------------
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+
+// console.log(main, secondary); //Garlic Bread Focaccia
+[main, secondary] = [secondary, main];
+console.log(main, secondary); //Garlic Bread Focaccia
+
+//--------------------------------------------------------------------------------------------------------
+//Destructuring an array 
+//--------------------------------------------------------------------------------------------------------
+console.log(restaurant.order(2,0)); //["Garlic Bread", "Pizza"], which is an array
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse); //Garlic Bread Pizza, which is separate element
+
+//--------------------------------------------------------------------------------------------------------
+//Nested Destructuring
+//--------------------------------------------------------------------------------------------------------
+const nested = [2, 4, [5,6]]; //array inside an array
+// const [i, ,j] = nested;
+// console.log(i, j); // (2), [5,6]
+
+const [i, ,[j,k]] = nested;
+console.log(i, j, k); //2 5 6
+
+//--------------------------------------------------------------------------------------------------------
+//Default Values
+//--------------------------------------------------------------------------------------------------------
+const [p=1, q=1, r=1] = [8, 9];
+console.log(p,q,r); //8 9 1
+
+
+
+
+
---
+++ Sec9Data_Structures_Operators/104destructuringObjects.js
@@ -0,0 +1,97 @@
+'use strict';
+//========================================================================================================
+//Section9 - 104 - Destructuring Objects
+//========================================================================================================
+/*
+Destructuring objects is very similar to destructuring arrays
+
+***IT'S EXTREMELY USEFUL WHEN USING API***
+
+*/
+
+
+// Data needed for first part of the section
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  order: function(starterIndex, mainIndex){
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]; //return an array
+  },
+
+  openingHours: {
+      thu: {
+          open: 12,
+          close: 22,
+      },
+      fri: {
+          open: 11,
+          close: 23,
+      },
+      sat: {
+          open: 0, //Open 24 hours
+          close: 24,
+      }
+  },
+
+  // ******VERY IMPORTANT*******
+  //function inside an object
+  //take obj as an param
+  //pro: without the need of passing many param into a function, instead just parse one obj in
+  orderDelivery: function( {starterIndex = 1, time = '11:00', address, mainIndex} ){ 
+      console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]}
+      will be delivered to ${address} at ${time}`);
+  },
+};
+
+//--------------------------------------------------------------------------------------------------------
+//Destructuring Objects
+//--------------------------------------------------------------------------------------------------------
+//simple destructuring
+const {name, openingHours, categories} = restaurant;
+console.log(name, openingHours, categories); 
+
+//setting "ALIAS"
+const {name: restaurantName, openingHours: hours, categories: tags} = restaurant;
+console.log(restaurantName, hours, tags);
+
+//assigning "DEFAULT VALUES" to objects that don't exist
+//assigning default value [] empty array
+const {menu = [], starterMenu: starter = [] } = restaurant;
+console.log(menu, starter); //[] ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"]
+
+//--------------------------------------------------------------------------------------------------------
+//Mutating Variables When Destructuring
+//--------------------------------------------------------------------------------------------------------
+let a = 111;
+let b = 999;
+
+const obj = {a: 23, b: 7, c: 14};
+
+//Destructuring elements in obj with the same variables name declared
+({a,b} = obj)
+console.log(a, b); //23 7
+
+//--------------------------------------------------------------------------------------------------------
+//Nested Objects
+//--------------------------------------------------------------------------------------------------------
+const {sat} = openingHours;
+console.log(sat); //{open: 0, close: 24}
+
+//put extra bracket 
+const {fri: {open: friOpen, close: friClose}} = openingHours;
+console.log(friOpen, friClose); //11 23
+
+// ******VERY IMPORTANT*******
+//work with function 
+//PASSING AN OBJECT AS A PARAM TO CALL A FUNCTION IN AN OBJECT
+restaurant.orderDelivery({
+    time: '22:30',
+    address: '123 abc street',
+    mainIndex:  2,
+    starterIndex: 2,
+}); //Order received! Garlic Bread and Risotto will be delivered to 123 abc street at 22:30
+
