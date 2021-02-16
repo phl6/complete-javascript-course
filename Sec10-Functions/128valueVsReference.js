5a49ae0d74d6675d27ffc03ce3ad6c896319d813
phl6 <guitarlouisli1997@gmail.com>
2021-02-16 15:55:52 +0800

    Sec128 Passing Arguments: Value vs. Reference

--- Sec10-Functions/128valueVsReference.js
+++ Sec10-Functions/128valueVsReference.js
@@ -7,18 +7,24 @@
 INTRODUCTION:
 
 
+-creating a copy, changing the copy won't affect the original variable
+-pass by value, since jonas is primitive data type
+
+const flightNum = flight; 
+
+-creating a copy, change the content of copy will ALSO change the content of the original object
+-pass by reference, since jonas is an object
+
+const passenger = jonas; 
+
+
 */
 
-
-//--------------------------------------------------------------------------------------------------------
-// 
-//--------------------------------------------------------------------------------------------------------
 
 const flight = 'LH234'; //primitive type, string
 const jonas = { //object
     name: 'Jonas Schmedtmann',
     passport: 24739479284,
-
 }
 
 const checkIn = function(flightNum, passenger){
@@ -26,7 +32,7 @@ const checkIn = function(flightNum, passenger){
     passenger.name = 'Mr. ' + passenger.name;
 
     if (passenger.passport === 24739479284){
-        alert('Check in')
+        console.log('Check in');
     }else{
         alert('Wrong Passport');
     }
@@ -34,12 +40,24 @@ const checkIn = function(flightNum, passenger){
 
 checkIn(flight, jonas)
 
-//VALUE
+//--------------------------------------------------------------------------------------------------------
+// PASS by VALUE
+//--------------------------------------------------------------------------------------------------------
 //Value DOESN'T change as flight is primitive type
 console.log(flight); // LH234
 
 //VS
 
+//--------------------------------------------------------------------------------------------------------
 //REFERENCE
-//Value DOES Changed as jonas is an Object
+//--------------------------------------------------------------------------------------------------------
+//jonas.name DOES Changed as jonas is an Object
 console.log(jonas);  // {name: "Mr. Jonas Schmedtmann", passport: 24739479284}
+
+
+const newPassport = function(person){
+    person.passport = Math.trunc(Math.random()*100000000000);    
+}
+
+newPassport(jonas); //changing the passport number
+checkIn(flight, jonas); //alert: Wrong Passport
