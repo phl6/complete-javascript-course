3c1e4452c1148be3f5ea2742a7a06dc5bc5efffd
phl6 <louisli1997@gmail.com>
2021-05-03 22:50:36 +0800

    Done 141 Simple array method, learnt slice(), splice(), reverse(), concat(), join()

--- Sec11-Arrays/141SimpleArrayMethod.js
+++ Sec11-Arrays/141SimpleArrayMethod.js
@@ -75,4 +75,67 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
+//--------------------------------------------------------------------------------------------------------------
+//slice()
+//doesn't mutate the array
+//--------------------------------------------------------------------------------------------------------------
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+//return element from 2nd position to end
+console.log(arr.slice(2)); //["c", "d", "e"]
+
+//return position 2-3 elements, note that 4th position element won't be returned
+console.log(arr.slice(2,4)); //["c", "d"]
+
+//return last element of the array
+console.log(arr.slice(-1)); //["e"]
+
+//return last 2 elements of the array
+console.log(arr.slice(-2)); //["d","e"]
+
+//return 1st to last 3 elements
+console.log(arr.slice(1, -2)); //["b", "c"]
+
+// arr.slice() vs [...arr] to create shallow copy
+// they are the same, just personal preference
+console.log(arr.slice()); //["a", "b", "c", "d", "e"]
+console.log([...arr]);    //["a", "b", "c", "d", "e"]
+
+//--------------------------------------------------------------------------------------------------------------
+//splice()
+//very similar to slice(), but splice instead "will mutate the array"
+//--------------------------------------------------------------------------------------------------------------
+console.log(arr.splice(2)); //["c", "d", "e"]
+console.log(arr); //["a","b"]
+
+//*** Commonly used
+//***remove the last element of an array
+arr.splice(-1);
+console.log(arr); //["a"]
+
+//--------------------------------------------------------------------------------------------------------------
+//reverse()
+//mutates the original array
+//--------------------------------------------------------------------------------------------------------------
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse()); //["f", "g", "h", "i", "j"]
+console.log(arr2); //["f", "g", "h", "i", "j"], original array has been mutated
+
+//--------------------------------------------------------------------------------------------------------------
+//concat()
+//--------------------------------------------------------------------------------------------------------------
+const letters = arr.concat(arr2);
+//approach1
+console.log(letters); //["a", "f", "g", "h", "i", "j"]
+
+//approach2
+console.log([...arr, ...arr2]); //["a", "f", "g", "h", "i", "j"]
+
+//--------------------------------------------------------------------------------------------------------------
+//join()
+//doesn't mutate the original array
+//--------------------------------------------------------------------------------------------------------------
+console.log(letters.join(' - ')); // a - f - g - h - i - j
+
+
 
