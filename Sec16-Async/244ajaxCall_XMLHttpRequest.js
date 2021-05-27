d3e6fa2d20a2262f081ea57f31c407846f690245
phl6 <guitarlouisli1997@gmail.com>
2021-05-27 17:53:01 +0800

    Done 244 ajax call XMLHttpRequest, built simple version of retrieving country data function

---
+++ Sec16-Async/244ajaxCall_XMLHttpRequest.js
@@ -0,0 +1,51 @@
+'use strict';
+
+//========================================================================================================
+//Section14 - 244 AJAX Call: XMLHttpRequest
+//========================================================================================================
+
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+///////////////////////////////////////
+
+//old school way doing Ajax
+
+const getCountryData = function(country){
+    const request = new XMLHttpRequest();
+    request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
+    request.send();
+    console.log(request.responseText);
+    
+    request.addEventListener('load', function(){
+        console.log(this.responseText);
+    
+        //destructure the response, which comes as an array
+        const [data] = JSON.parse(this.responseText);
+    
+        console.log(data);
+    
+        const html = `
+            <article class="country">
+            <img class="country__img" src=${data.flag} />
+            <div class="country__data">
+                <h3 class="country__name">${data.name}</h3>
+                <h4 class="country__region">${data.region}</h4>
+                <p class="country__row"><span>👫</span>${(data.population / 1000000).toFixed(1)} million people</p>
+                <p class="country__row"><span>🗣️</span>${data.languages[0].nativeName}</p>
+                <p class="country__row"><span>${data.currencies[0].symbol}</span>${data.currencies[0].name}</p>
+            </div>
+            </article>
+        `
+    
+        countriesContainer.insertAdjacentHTML('beforeend', html);
+    })
+}
+
+getCountryData('kazakhstan');
+getCountryData('Japan');
+getCountryData('uzbekistan');
+getCountryData('Christmas Island');
+getCountryData('Gibraltar');
+getCountryData('turkey');
