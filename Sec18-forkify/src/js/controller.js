857e64b709861b535bb5d763d76b6547d2c69d27
phl6 <guitarlouisli1997@gmail.com>
2021-06-17 16:30:32 +0800

    Done 282 Loading a Recipe from API

--- Sec18-forkify/src/js/controller.js
+++ Sec18-forkify/src/js/controller.js
@@ -10,4 +10,36 @@ const timeout = function (s) {
 
 // https://forkify-api.herokuapp.com/v2
 
-///////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//282 Loading a Recipe from API
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//fetch data from API 
+const showRecipe = async function(){
+  try {
+    //Retrieve data from API
+    const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bcd09');
+    const data = await res.json();
+
+    if(!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    //Destructure and Reformat data into recipe
+    let {recipe} = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceURL: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+
+    console.log(recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+showRecipe();
